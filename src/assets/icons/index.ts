a7c020dba348c7d4d87499d0ea5dda2221705cf3
harshvishwakarma443 <harsh.vishwakarma@idsspl.com>
2026-07-16 04:12:02 +0530

    applied report

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,12 +3,14 @@ import person from "./person.svg";
 import addPerson from "./add-person.svg";
 import pdf from "./pdf.svg";
 import xls from "./xsl.svg";
+import txt from "./txt.svg";
 
 // Export as ICON object with properties
 export const ICONS = {
   PERSON: person,
   PDF: pdf,
   XLS: xls,
+  TXT: txt,
   ADD_PERSON: addPerson,
 } as const;
 
